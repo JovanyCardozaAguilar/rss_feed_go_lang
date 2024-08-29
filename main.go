03c6f8500fc4814217167c2f94dd74e4933a8bfb
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2024-08-29 14:00:06 -0700

    can pull videos from a specified youtube channel

--- main.go
+++ main.go
@@ -1,45 +1,115 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/xml"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
-const URL = "https://jsonplaceholder.typicode.com/posts"
-
-type Posts struct {
-	Posts []Post `json:"posts"`
+// Define the structure of the YouTube RSS feed
+type Feed struct {
+	XMLName xml.Name `xml:"feed"`
+	Entries []Entry  `xml:"entry"`
 }
 
-type Post struct {
-	UserId int    `json:"userId"`
-	Id     int    `json:"id"`
-	Title  string `json:"title"`
-	Body   string `json:"body"`
+type Entry struct {
+	ID        string `xml:"id"`
+	VideoId   string `xml:"videoId"`
+	ChannelId string `xml:"channelId"`
+	Title     string `xml:"title"`
+	Link      Link   `xml:"link"`
+	Author    Author `xml:"author"`
+	Published string `xml:"published"`
+	Updated   string `xml:"updated"`
+	Group     Group  `xml:"group"`
 }
 
-func GetJson(url string, data interface{}) error {
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-	return json.NewDecoder(response.Body).Decode(data)
+type Link struct {
+	Rel  string `xml:"rel,attr"`
+	Href string `xml:"href,attr"`
 }
 
-func GetPost() {
-	var posts []Post
-	err := GetJson(URL, &posts)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	} else {
-		for i := 0; i < len(posts); i++ {
-			fmt.Println(posts[i].Title)
-		}
-	}
+type Author struct {
+	Name string `xml:"name"`
+	URI  string `xml:"uri"`
+}
+
+type Group struct {
+	Title       string    `xml:"title"`
+	Content     Content   `xml:"content"`
+	Thumbnail   Thumbnail `xml:"thumbnail"`
+	Description string    `xml:"description"`
+	Community   Community `xml:"community"`
+}
+
+type Content struct {
+	URL    string `xml:"url,attr"`
+	Type   string `xml:"type,attr"`
+	Width  string `xml:"width,attr"`
+	Height string `xml:"height,attr"`
+}
+
+type Thumbnail struct {
+	URL    string `xml:"url,attr"`
+	Width  string `xml:"width,attr"`
+	Height string `xml:"height,attr"`
+}
+
+type Community struct {
+	StarRating StarRating `xml:"starRating"`
+	Statistics Statistics `xml:"statistics"`
+}
+
+type StarRating struct {
+	Count   string `xml:"count,attr"`
+	Average string `xml:"average,attr"`
+	Min     string `xml:"min,attr"`
+	Max     string `xml:"max,attr"`
+}
+
+type Statistics struct {
+	Views string `xml:"views,attr"`
 }
 
 func main() {
-	GetPost()
+	// YouTube channel url
+	url := "https://www.youtube.com/feeds/videos.xml?channel_id=UCr3L56DsMxhNtHqtm2AIsMw"
+
+	// Fetch the RSS feed
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error fetching RSS feed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+
+	// Print the raw XML response for debugging
+	// fmt.Println("Raw XML Response:")
+	// fmt.Println(string(body))
+
+	// Parse the RSS feed
+	var feed Feed
+	err = xml.Unmarshal(body, &feed)
+	if err != nil {
+		log.Fatalf("Error parsing XML: %v", err)
+	}
+
+	// Print each entry
+	for _, entry := range feed.Entries {
+		fmt.Println("Author:", entry.Author.Name)
+		fmt.Println("Title:", entry.Title)
+		fmt.Println("Link:", entry.Link.Href)
+		fmt.Println("Published:", entry.Published)
+		fmt.Println("Updated:", entry.Updated)
+		fmt.Println("Description:", entry.Group.Description)
+		fmt.Println("Thumbnail URL:", entry.Group.Thumbnail.URL)
+		fmt.Println()
+	}
 }
